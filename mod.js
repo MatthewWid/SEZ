440049dc44a20ff014725dce243b81fa4c1a6b66
MatthewMob <game1000@hotmail.co.uk>
2015-07-25 22:20:34 +1000

    Made mod easier to modify by putting config vars into object

--- mod.js
+++ mod.js
@@ -1,7 +1,5 @@
 /*
 	
-	Hi!
-	
 	S.E.Z. - SifiGame EZ
 	
 	This mod was produced and developed by Matthew W.
@@ -12,43 +10,35 @@
 	
 */
 
+console.log("Mod Started");
+
 // Config Vars
-var minimap_icon_size = 2;
-	minimap_icon_color_enemy = "red";
-	minimap_icon_color_self = "white";
-	minimap_border = "blue";
-	minimap_background = "black";
-	minimap_bordercurve = "200";
-	minimap_opacity = 0.7;
-	minimap_width = 200;
-	minimap_height = 200;
-var health_color = "white";
-/*
-Defaults:
-var minimap_icon_size = 2;
-	minimap_icon_color_enemy = "red";
-	minimap_icon_color_self = "white";
-	minimap_border = "blue";
-	minimap_background = "black";
-	minimap_bordercurve = "200";
-	minimap_opacity = 0.7;
-	minimap_width = 200;
-	minimap_height = 200;
-var health_color = "white";
-*/
+var mod = {
+	enabled: true,
+	minimap_icon_size: 2,
+	minimap_icon_color_enemy: "red",
+	minimap_icon_color_self: "white",
+	minimap_border: "blue",
+	minimap_background: "black",
+	minimap_bordercurve: "200",
+	minimap_opacity: 0.7,
+	minimap_width: 200,
+	minimap_height: 200,
+	health_color: "white"
+}
 
 // Set Properties of and Create Canvas
 var canvas = document.createElement('canvas');
 canvas.id     = "minimap";
-canvas.width  = minimap_width;
-canvas.height = minimap_height;
+canvas.width  = mod.minimap_width;
+canvas.height = mod.minimap_height;
 canvas.style.zIndex   = 999;
 canvas.style.position = "absolute";
-canvas.style.border   = "1px solid "+minimap_border;
-canvas.style.borderRadius = minimap_bordercurve+"px";
-canvas.style.background = minimap_background;
+canvas.style.border   = "1px solid "+mod.minimap_border;
+canvas.style.borderRadius = mod.minimap_bordercurve+"px";
+canvas.style.background = mod.minimap_background;
 canvas.style.bottom = "0px";
-canvas.style.opacity = minimap_opacity;
+canvas.style.opacity = mod.minimap_opacity;
 document.body.appendChild(canvas);
 
 // Get element and get context of minimap canvas
@@ -70,11 +60,11 @@ function lineDistance( point1, point2 )
   return Math.sqrt( xs + ys );
 }
 
-function sez(game) {
+function sez() {
 	
 	// Draw Banner and Health
 	ctx.save();
-	ctx.fillStyle = health_color;
+	ctx.fillStyle = mod.health_color;
 	ctx.fillText("Running S.E.Z. by Matthew W.", game.canvastranslatex+cwidth/2-150, game.canvastranslatey+cheight-600);
 	ctx.fillText(playercollection.array[0].health, game.canvastranslatex+cwidth/2-656, game.canvastranslatey+cheight-613)
 	ctx.restore();
@@ -83,24 +73,24 @@ function sez(game) {
 	mm.clearRect(0, 0, minimap.width, minimap.height);
 	
 	// Draw Self On Minimap
-	mm.fillStyle = minimap_icon_color_self;
-	mm.strokeStyle = minimap_icon_color_self;
+	mm.fillStyle = mod.minimap_icon_color_self;
+	mm.strokeStyle = mod.minimap_icon_color_self;
 	mm.beginPath();
-	mm.arc(playercollection.array[0].x/16+50, playercollection.array[0].y/16+75,minimap_icon_size,0,2*Math.PI);
+	mm.arc(playercollection.array[0].x/16+50, playercollection.array[0].y/16+75, mod.minimap_icon_size, 0, 2*Math.PI);
 	mm.fill();
 	mm.stroke();
 	
-	if (game.running) {
+	if (game2.running) {
 		
 		// Set up colours for enemies and start for loop
 		ctx.save();
-		mm.fillStyle = minimap_icon_color_enemy;
-		mm.strokeStyle = minimap_icon_color_enemy;
+		mm.fillStyle = mod.minimap_icon_color_enemy;
+		mm.strokeStyle = mod.minimap_icon_color_enemy;
 		for (var i = 2; i < playercollection.array.length; i++) {
 			
 			// Draw Enemies
 			mm.beginPath();
-			mm.arc(playercollection.array[i].x/16+50, playercollection.array[i].y/16+75,minimap_icon_size,0,2*Math.PI);
+			mm.arc(playercollection.array[i].x/16+50, playercollection.array[i].y/16+75, mod.minimap_icon_size, 0, 2*Math.PI);
 			mm.fill();
 			mm.stroke();
 			
